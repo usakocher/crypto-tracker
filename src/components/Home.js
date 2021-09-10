9fed1115180d94a2a36917ad404722bc6746971d
usakocher <adammkocher@gmail.com>
2021-09-10 00:32:58 -0400

    Adding Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,32 @@
 import {makeStyles} from '@material-ui/core';
+import showcaseImg from '../assets/images/Stocksy_comp_1811893.jpg';
 
 const useStyles = makeStyles({
-
+    header: {
+        textAlign: 'center',
+        marginTop: '4vh',
+        fontSize: '25px'
+    },
+    p:{
+        fontSize: '25px'
+    },
+    showcase: {
+        background: `url(${showcaseImg})`,
+        width: '100%',
+        height: '90vh',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+    },
+    aside: {
+        width: '30%',
+        marginTop: '12%',
+        paddingLeft: '0.5rem',
+        paddingRight: '2rem',
+        float: 'right'
+    },
+    asideP: {
+        margin: '0.5rem'
+    }
 })
 
 export const Home = () => {
@@ -9,7 +34,17 @@ export const Home = () => {
     
     return (
         <div>
-           Home 
+            <div className={classes.header}>
+                <h1>Welcome to the Crypto-Tracker Experience</h1>
+                <p className={classes.p}>Here you can learn about Cryptocurrencies, track them, and find places to buy them.</p>
+            </div>
+            <div className={classes.showcase}>
+                <aside className={classes.aside}>
+                    <p className={classes.asideP}>More than 1 in 10 Americans invested in cryptocurrency over the past year, according to a survey published by the University of Chicago, a sign of the popularity of digital currencies like bitcoin and ethereum. Is it time for you?</p>
+                    <br/>
+                    <p className={classes.asideP}>Here at CTE we strive to give you the tools necessary to build your future through cryptocurrency. We want to create a secure environment so that you have all the information and can make the best decisions. The future of investing is calling, don't send it straight to voicemail.</p>
+                </aside>
+            </div>
         </div>
     )
 }
