15cd8fef8618cf0821599f919b6b3471de61e821
usakocher <adammkocher@gmail.com>
2021-09-10 07:27:35 -0400

    formatted table

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -77,6 +77,7 @@ export const Tracker = () => {
         axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
         .then(res => {
             setCoins(res.data)
+            console.log(res.data)
         }).catch(error => console.log(error))
         }, []);
     
